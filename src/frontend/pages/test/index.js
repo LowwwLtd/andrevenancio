f4b42593ac56cd05380d3450af42c1f1d858dd81
andrevenancio <noreply@andrevenancio.com>
2020-04-01 13:09:13 +0100

    test

--- src/frontend/pages/test/index.js
+++ src/frontend/pages/test/index.js
@@ -3,17 +3,23 @@ import './style.scss';
 
 export const TestPage = () => {
     return (
-        <div class="container">
-            <h1>Parallax</h1>
-            <h2>faster</h2>
-            <h2>easier</h2>
-            <h2>sassier</h2>
-            <img src="http://placekitten.com/200/200" alt=" " />
-            <img src="http://placekitten.com/150/200" alt=" " />
-            <img src="http://placekitten.com/200/150" alt=" " />
-            <img src="http://placekitten.com/150/250" alt=" " />
-            <img src="http://placekitten.com/300/400" alt=" " />
-            <img src="http://placekitten.com/400/300" alt=" " />
+        <div class="base-wrapper">
+            <div class="parallax-container">
+                <div class="wrapper">
+                    <img
+                        class="background__image"
+                        src="https://s3-ap-southeast-2.amazonaws.com/daily-fire-assets/codepen-assets/dotted-background.png"
+                    />
+                    <img
+                        class="middle__image"
+                        src="https://s3-ap-southeast-2.amazonaws.com/daily-fire-assets/codepen-assets/stars.png"
+                    />
+                    <img
+                        class="foreground__image"
+                        src="https://s3-ap-southeast-2.amazonaws.com/daily-fire-assets/codepen-assets/polygons.png"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
